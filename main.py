9ab2ce282c5258c378602dc8a1b5c97a1bbc7875
jose906 <jrestenssoro@hotmail.com>
2026-02-19 17:06:32 -0400

    nuevos

--- main.py
+++ main.py
@@ -161,6 +161,7 @@ def fetch_tweets_for_user(username: str, last_tweetid ):
 
     
 def update_last_tweetid(idTweetUser, last_tweetid):
+    conn = None
     conn = get_db_connection()
     cur = conn.cursor(dictionary=True)
     sql = "UPDATE TweetUser SET last_tweetid = %s, tweets_procesados = 1 WHERE idTweetUser = %s"
